a5a4f7f1de3cc12541d9340087eed6e39ab339cd
Marie <mariepohlak@Marie.localdomain>
2025-08-01 19:49:49 +0300

    Created a Rot13 class

---
+++ Rot13.java
@@ -0,0 +1,15 @@
+public class Rot13 {
+
+    public static String encrypt(String text) {
+        String result = "";
+
+
+        return result;
+    }
+    public static void main(String[] args) {
+       String encryptedmessage = Rot13.encrypt("Päike paistab");
+
+        System.out.println(encryptedmessage);
+    
+    }
+}

286aa050bf113926531b00dff63bffed0a6dfaa1
karinhakmann1 <karinhakmann@gmail.com>
2025-08-01 19:40:13 +0300

    my first programm

---
+++ Atbash.java
@@ -0,0 +1,15 @@
+public class Atbash{
+    public static String encrypt(String text) {
+        String result = "";
+
+
+
+        return result;
+    }
+    public static void main(String[] args) {
+
+        String encryptedMessage = Atbash.encrypt("The weather is nice");
+
+        System.out.println(encryptedMessage);
+    }
+}

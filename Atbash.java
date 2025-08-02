f665ff26e0b88e22037d7d2874c7511ad2bb7283
karinhakmann1 <karinhakmann@gmail.com>
2025-08-02 15:06:33 +0300

    learning git

--- Atbash.java
+++ Atbash.java
@@ -2,10 +2,18 @@ public class Atbash{
     public static String encrypt(String text) {
         String result = "";
 
+        for (int i = 0; i < text.length(); i++) {
+            char character = text.charAt(i);
+
+            
+
+            result = result + character;
+
+        }
 
 
-        return result;
-    }
+         return result;
+    } 
     public static void main(String[] args) {
 
         String encryptedMessage = Atbash.encrypt("The weather is nice");

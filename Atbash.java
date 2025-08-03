0ce9f8d0eecaaefc06e79c34fcfdfad3470a453b
karinhakmann1 <karinhakmann@gmail.com>
2025-08-03 11:39:47 +0300

    Minu Atbash

--- Atbash.java
+++ Atbash.java
@@ -5,15 +5,23 @@ public class Atbash{
         for (int i = 0; i < text.length(); i++) {
             char character = text.charAt(i);
 
+            if (Character.isUpperCase(character)){
+               character = (char) ('Z' - (character - 'A'));  
+              
+            } else if (Character.isLowerCase (character)) {
+                 character = (char) ('z' - (character - 'a'));
+            }
+
+            
 
 
             result = result + character;
 
         }
 
-
-         return result;
-    } 
+          return result;
+        }
+    
     public static void main(String[] args) {
 
         String encryptedMessage = Atbash.encrypt("The weather is nice");
